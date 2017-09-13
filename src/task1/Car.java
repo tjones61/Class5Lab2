7a4fccec0f0afad480db7a0e8afbfb915842201c
tjones61 <tjones61@10.18.12.31>
2017-09-13 14:02:44 -0500

    addition of Car body

--- src/task1/Car.java
+++ src/task1/Car.java
@@ -10,5 +10,30 @@ package task1;
  * @author tjones61
  */
 public class Car {
+    private int doors;
+    private int cylinders;
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public void setDoors(int doors) {
+        if(doors<2||doors>4||doors%2 !=0){
+            throw new IllegalArgumentException("This is not a valid amount of doors");
+        }
+        this.doors = doors;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public void setCylinders(int cylinders) {
+        if(cylinders<3||cylinders>16){
+            throw new IllegalArgumentException("This is not a valid amount of cylinders");
+        }
+        this.cylinders = cylinders;
+    }
+
     
 }
