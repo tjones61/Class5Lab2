46a984dea22f40bef13baa407a3f25d8fce930be
tjones61 <tjones61@10.18.12.31>
2017-09-13 15:12:35 -0500

    task 2 bodies and abstract

--- src/task2/Car.java
+++ src/task2/Car.java
@@ -9,6 +9,31 @@ package task2;
  *
  * @author tjones61
  */
-public class Car {
+public abstract class Car {
+    private int doors;
+    private int cylinders;
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public void setDoors(int doors) {
+        if(doors!= 2||doors!=4){
+            throw new IllegalArgumentException("This is not a valid amount of doors");
+        }
+        this.doors = doors;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public void setCylinders(int cylinders) {
+        if(cylinders<3||cylinders>16){
+            throw new IllegalArgumentException("This is not a valid amount of cylinders");
+        }
+        this.cylinders = cylinders;
+    }
+
     
 }
--- src/task2/Honda.java
+++ src/task2/Honda.java
@@ -9,6 +9,17 @@ package task2;
  *
  * @author tjones61
  */
-public class Honda {
-    
+public abstract class Honda extends Car{
+    private String wheelDrive;
+
+    public String getWheelDrive() {
+        return wheelDrive;
+    }
+
+    public void setWheelDrive(String wheelDrive) {
+        if(!(wheelDrive.equals("four"))||!(wheelDrive.equals("front"))||!(wheelDrive.equals("rear"))){
+            throw new IllegalArgumentException("This is not a valid wheel drive");
+        }
+        this.wheelDrive = wheelDrive;
+    }
 }
